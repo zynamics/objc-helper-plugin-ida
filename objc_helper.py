ff37d8bfe5f3d9191bd2ad8ade35509b062e7137
Jose Duart <tagetora@gmail.com>
2010-05-25 12:46:12 +0200

    Minor changes and bugfixes:
      - Fixed a problem when tracking R0 register that was modified by
      previous calls. Now if the script is tracking R0 and finds a BX/BLX,
      it assumes that is modifying R0 and stops, marking the tracking as
      failed.
    
      - Changed the way the script parses the data references so it works
      both with release and debug binaries. Instead of getting the raw
      offset we now use recursive calls to idautils.DataRefsFrom().
      For the references to work properly we had to make a pre-process
      converting all dwords to offsets in the classrefs and superrefs
      sections.
    
      - In some cases, compiler can decide to use LR as a general register
      so the search for R0..R15 fails. Now the script includes the
      handling of this special case.
    
      - Added check of Thumb/non-Thumb code for patching the calls correctly.
    
      - Fixed bug that was getting the incorrect parameters for other
      flavours of msgSend(). Now it should be easier to add others.

--- objc_helper.py
+++ objc_helper.py
@@ -5,7 +5,6 @@ import time
 displ_re = re.compile('\[R(?P<regnum>\d+)')
 var_re = re.compile(', \[SP,#0x.*\+(?P<varname>\w+)\]')
 
-
 def trace_param(ea, min_ea, op_type, op_val):
     '''
     trace_param: ea, min_ea, op_type, op_val
@@ -23,6 +22,13 @@ def trace_param(ea, min_ea, op_type, op_val):
     ea_call = ea
     while ea != idc.BADADDR and ea != min_ea:
         ea = idc.PrevHead(ea, min_ea)
+        
+        if op_type == idaapi.o_reg and op_val == 0 and idaapi.is_call_insn(ea):
+            # We have a BL/BLX that will modify the R0
+            # we're tracking
+            #
+            return None
+
         if idc.GetMnem(ea) in ['LDR', 'MOV']:
             src_op = 1
             dest_op = 0
@@ -32,12 +38,19 @@ def trace_param(ea, min_ea, op_type, op_val):
         else:
             continue
 
+
         if idc.GetOpType(ea, dest_op) == op_type and idc.GetOperandValue(ea, dest_op) == op_val:
             # Found, see where it comes from
             if idc.GetOpType(ea, src_op) == idc.o_mem:
                 # Got the final reference
-                local_ref = idc.GetOperandValue(ea, 1)
-                far_ref = idc.Dword(local_ref)
+                refs = list(idautils.DataRefsFrom(ea))
+                if not refs:
+                    local_ref = idc.GetOperandValue(ea, src_op)
+                    far_ref = idc.Dword(local_ref)
+                else:
+                    while len(refs) > 0:
+                        far_ref = refs[0]
+                        refs = list(idautils.DataRefsFrom(refs[0]))
                 return far_ref
             elif idc.GetOpType(ea, src_op) == idc.o_displ:
                 if ', [SP' in idc.GetDisasm(ea):
@@ -46,33 +59,48 @@ def trace_param(ea, min_ea, op_type, op_val):
                         return None
 
                     # We're tracking an stack variable
-                    var_name = var_re.search(idc.GetDisasm(ea)).group('varname')
+                    try:
+                        var_name = var_re.search(idc.GetDisasm(ea)).group('varname')
+                    except:
+                        print '%08x: Unable to recognize variable'
+                        return None
 
                     while ea != idc.BADADDR and ea > min_ea:
                         if idc.GetMnem(ea) == 'STR' and var_name in idc.GetDisasm(ea):
                             # New reg to track
-                            op_val = idc.GetOperandValue(ea, 0)
+                            op_val = idc.GetOperandValue(ea, dest_op)
                             break
                         ea = idc.PrevHead(ea, min_ea)
                 else:
                     # New reg to track
-                    op_val = int(displ_re.search(idc.GetDisasm(ea)).group('regnum'))
+                    if '[LR]' in idc.GetDisasm(ea):
+                        # Optimizations use LR as general reg
+                        op_val = 14
+                    else:
+                        try:
+                            op_val = int(displ_re.search(idc.GetDisasm(ea)).group('regnum'))
+                        except:
+                            print '%08x: Unable to recognize register' % ea
+                            return None    
             elif idc.GetOpType(ea, src_op) == idc.o_reg:
                 # Direct reg-reg assignment
                 op_val = idc.GetOperandValue(ea, src_op)
+            else:
+                # We don't track o_phrase or other complex source operands :(
+                return None
     return None
         
     
 
-def fix_callgraph(msgsend, segname):
+def fix_callgraph(msgsend, segname, class_param, sel_param):
     '''
-    fix_callgraph: msgsend, segname
+    fix_callgraph: msgsend, segname, class_param, sel_param
     
     Given the msgsend flavour address as a parameter, looks
-    for the parameters (class and selector) and creates a new
-    segment where it places a set of dummy calls named as
-    classname_methodname (we use method instead of selector
-    most of the time).
+    for the parameters (class and selector, identified by
+    class_param and sel_param) and creates a new segment where
+    it places a set of dummy calls named as classname_methodname
+    (we use method instead of selector most of the time).
     '''
     
     t1 = time.time()
@@ -91,18 +119,16 @@ def fix_callgraph(msgsend, segname):
         if not func_start or func_start == idc.BADADDR:
             continue
         ea = ea_call
-        r1_ref = trace_param(ea, func_start, idc.o_reg, 1)
-        if r1_ref:
-            method_name_ea = idc.Dword(r1_ref)
+        method_name_ea = trace_param(ea, func_start, idc.o_reg, sel_param)
+        if method_name_ea:
             method_name = idc.GetString(method_name_ea, -1, idc.ASCSTR_C)
             if not method_name:
                 method_name = ''
         else:
             method_name = ''
 
-        r0_ref = trace_param(ea, func_start, idc.o_reg, 0)
-        if r0_ref:
-            class_name_ea = idc.Dword(r0_ref)
+        class_name_ea = trace_param(ea, func_start, idc.o_reg, class_param)
+        if class_name_ea:
             class_name = idc.Name(class_name_ea)
             if not class_name:
                 class_name = ''
@@ -126,6 +152,10 @@ def fix_callgraph(msgsend, segname):
     print '\nFinal stats:\n\t%d total calls, %d resolved' % (total, resolved)
     print '\tAnalysis took %.2f seconds' % (time.time() - t1)
 
+    if resolved == 0:
+        print 'Nothing to patch.'
+        return
+        
     print 'Adding new segment to store new nullsubs'
 
     # segment size = opcode ret (4 bytes) * num_calls  
@@ -147,21 +177,43 @@ def fix_callgraph(msgsend, segname):
             idc.MakeRptCmt(seg_ptr, new_name)
             offset = seg_ptr - ea
             seg_ptr += 4
-        
+
         # patch the msgsend call
-        if offset > 0 and offset & 0xFF800000:
-            print 'Offset too far (%08x) Stopping [%08x]' % (offset, ea)
-            return
-        off1 = (offset & 0x7FF000) >> 12
-        off2 = (offset & 0xFFF) / 2
-        w1 = (0xF000 | off1)
-        w2 = (0xE800 | off2) - 1
-        idc.PatchWord(ea, w1)
-        idc.PatchWord(ea + 2, w2)
-        
+        if idc.GetReg(ea, "T") == 1:
+            if offset > 0 and offset & 0xFF800000:
+                print 'Offset too far for Thumb (%08x) Stopping [%08x]' % (offset, ea)
+                return
+    
+            off1 = (offset & 0x7FF000) >> 12
+            off2 = (offset & 0xFFF) / 2
+            w1 = (0xF000 | off1)
+            w2 = (0xE800 | off2) - 1
+            idc.PatchWord(ea, w1)
+            idc.PatchWord(ea + 2, w2)
+        else:
+            if offset > 0 and offset & 0xFF000000:
+                print 'Offset too far (%08x) Stopping [%08x]' % (offset, ea)
+            dw = (0xFA000000 | (offset - 8 >> 2))
+            if dw < 0:
+                dw = dw & 0xFAFFFFFF
+            idc.PatchDword(ea, dw)                
+
+
+def make_offsets(segname):
+    segea = idc.SegByName(segname)
+    segend = idc.SegEnd(segea)
+    
+    while segea < segend:
+        idc.OpOffset(segea, 0)
+        ptr = idc.Dword(segea)
+        idc.OpOffset(ptr, 0)
+        segea += 4
             
 if __name__ == '__main__':
-    fix_callgraph(idc.LocByName('_objc_msgSend'), 'msgSend')
-    fix_callgraph(idc.LocByName('_objc_msgSend_stret'), 'msgSendstret')
-    fix_callgraph(idc.LocByName('_objc_msgSendSuper2'), 'msgSendSuper')
+    make_offsets('__objc_classrefs')
+    make_offsets('__objc_superrefs')
+    idaapi.analyze_area(idc.MinEA(), idc.MaxEA())
+    fix_callgraph(idc.LocByName('_objc_msgSend'), 'msgSend', 0, 1)
+    fix_callgraph(idc.LocByName('_objc_msgSendSuper2'), 'msgSendSuper', 3, 1)
+    idaapi.analyze_area(idc.MinEA(), idc.MaxEA())
     print 'Done.'
