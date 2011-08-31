8105eec0ee00eecf605fbcf1eaf561e137576a2a
Jose Duart <jose.duart@zynamics.com>
2011-08-31 10:17:32 +0200

    Fixes and sanity checks. See changelog for details.

--- objc_class.py
+++ objc_class.py
@@ -31,7 +31,7 @@ class ObjcProperties:
 #        print '%08x: Parsing properties' % ea
         entry_size = idc.Dword(ea)
         num_entries = idc.Dword(ea + 4)
-        
+
         ea = ea + 8
         for i in range(num_entries):
             var_name = idc.GetString(idc.Dword(ea), -1, idc.ASCSTR_C)
@@ -39,7 +39,7 @@ class ObjcProperties:
             self.property_list.append({'name':var_name, 'type':var_type})
             ea = ea + entry_size
         return
-        
+
     def __len__(self):
         return len(self.property_list)
 
@@ -57,7 +57,7 @@ class ObjcIvars:
 #        print '%08x: Parsing ivars' % ea
         entry_size = idc.Dword(ea)
         num_entries = idc.Dword(ea + 4)
-        
+
         ea = ea + 8
         for i in range(num_entries):
             ivar_offset = idc.Dword(idc.Dword(ea))
@@ -66,7 +66,7 @@ class ObjcIvars:
             self.ivar_list.append({'name':ivar_name, 'type':ivar_type, 'offset':ivar_offset})
             ea = ea + entry_size
         return
-        
+
     def __len__(self):
         return len(self.ivar_list)
 
@@ -84,7 +84,7 @@ class ObjcMethods:
 #        print '%08x: Parsing methods' % ea
         entry_size = idc.Dword(ea)
         num_entries = idc.Dword(ea + 4)
-        
+
         ea = ea + 8
         for i in range(num_entries):
             method_name = idc.GetString(idc.Dword(ea), -1, idc.ASCSTR_C)
@@ -102,7 +102,7 @@ class ObjcMethods:
         for entry in self.method_list:
             dump += '%08x: %s\n' % (entry['addr'], decode_type(entry['name'], entry['type']))
         return dump
-        
+
 
 class ObjcProtocols:
     def __init__(self, ea):
@@ -111,29 +111,29 @@ class ObjcProtocols:
 #        print '%08x: Parsing protocols' % ea
         num_main_structs = idc.Dword(ea)
         ea = ea + 4
-        
+
         for i in range(num_main_structs):
             struct_off = idc.Dword(ea)
             protocol_name = idc.GetString(idc.Dword(struct_off + 4), -1, idc.ASCSTR_C)
             protocol_list = idc.Dword(struct_off + 8)
             instance_methods = idc.Dword(struct_off + 0xC)
             class_methods = idc.Dword(struct_off + 0x14)
-            
+
             if instance_methods:
                 _inst = ObjcMethods(instance_methods)
             else:
                 _inst = None
-            
+
             if class_methods:
                 _class = ObjcMethods(class_methods)
             else:
                 _class = None
-    
+
             if protocol_list:
                 _meta = ObjcProtocols(protocol_list)
             else:
                 _meta = None
-            
+
             self.protocol_list.append({
                 'name': protocol_name,
                 'instance_methods': _inst,
@@ -156,53 +156,61 @@ class ObjcProtocols:
             if entry['class_methods'] and len(entry['class_methods']):
                 dump += '  Class Methods:\n    '
                 dump += repr(entry['class_methods']).replace('\n', '\n    ')
-                
+
             if entry['meta_protocols'] and len(entry['meta_protocols']):
                 dump += '  Protocol list:\n    '
                 dump += repr(entry['meta_protocols']).replace('\n', '\n    ')
 
         return dump
-        
+
 
 class ObjcClass:
     def __init__(self, ea):
         self.class_info = dict()
-        
+
+        objc_const_seg = idc.SegByBase(idc.SegByName('__objc_const'))
+        objc_const_end = idc.SegEnd(objc_const_seg)
+
         self.class_info['meta_class'] = idc.Dword(ea)
         self.class_info['super_class'] = idc.Dword(ea + 4)
         self.class_info['cache'] = idc.Dword(ea + 8)
         self.class_info['vtable'] = idc.Dword(ea + 0xC)
         _class_def = idc.Dword(ea + 0x10)
-        
+
+        if _class_def < objc_const_seg or _class_def > objc_const_end:
+            return
+
         self.class_info['top_class'] = idc.Dword(_class_def)
         self.class_info['instance_size'] = idc.Dword(_class_def + 8)
-        
+
         name_off = idc.Dword(_class_def + 0x10)
         class_name = idc.GetString(name_off, -1, idc.ASCSTR_C)
         if not class_name:
             class_name = '[UNKNOWN]'
         self.class_info['name'] = class_name
-        
+
         self.class_info['methods'] = list()
         self.class_info['protocols'] = list()
         self.class_info['ivars'] = list()
         self.class_info['properties'] = list()
-        
+
         if idc.Dword(_class_def + 0x14):
             self.class_info['methods'] = ObjcMethods(idc.Dword(_class_def + 0x14))
-            
+
         if idc.Dword(_class_def + 0x18):
             self.class_info['protocols'] = ObjcProtocols(idc.Dword(_class_def + 0x18))
-            
+
         if idc.Dword(_class_def + 0x1C):
             self.class_info['ivars'] = ObjcIvars(idc.Dword(_class_def + 0x1C))
-            
+
         if idc.Dword(_class_def + 0x24):
             self.class_info['properties'] = ObjcProperties(idc.Dword(_class_def + 0x24))
-        
+
         return
 
     def dump(self):
+        if not self.class_info.has_key('name'):
+            return
         print 'Class: %s' % self.class_info['name']
         print 'Attributes:'
         print '  IsTopClass: %d' % self.class_info['top_class']
@@ -211,10 +219,10 @@ class ObjcClass:
         print '  Protocols: %d' % len(self.class_info['protocols'])
         print '  Instance Vars: %d' % len(self.class_info['ivars'])
         print '  properties: %d' % len(self.class_info['properties'])
-        
+
         if len(self.class_info['methods']):
             print 'Method list:\n  %s' % repr(self.class_info['methods']).replace('\n', '\n  ')
-        
+
         if len(self.class_info['protocols']):
             print 'Protocols:\n  %s' % repr(self.class_info['protocols']).replace('\n', '\n  ')
 
@@ -226,48 +234,26 @@ class ObjcClass:
 
 
 def main():
-    objc_data_seg = idc.SegByName('__objc_data')
+    objc_data_seg = idc.SegByBase(idc.SegByName('__objc_data'))
     if objc_data_seg == idc.BADADDR:
         print 'Cannot locate objc_data segment'
         return
-    
+
     ea = objc_data_seg
     while ea < idc.SegEnd(objc_data_seg):
         objc_class = ObjcClass(ea)
         objc_class.dump()
-        '''
-        class_struct = parse_class_struct(ea)
-        if class_struct.get('class_def'):
-            class_def = parse_class_def(class_struct['class_def'])
-        else:
-            print 'No class definition found'
-            continue
-            
-        if class_def.get('method_struct'):
-            parse_method_struct(class_def['method_struct'])
-
-        if class_def.get('meta_protocols'):
-            parse_protocols_list(class_def['meta_protocols'])
-
-        if class_def.get('ivars_struct'):
-            parse_ivars_struct(class_def['ivars_struct'])
-            
-        if class_def.get('properties'):
-            parse_properties(class_def['properties'])
-'''
         ea = ea + 0x14
-#        if ea > objc_data_seg + 0x15:
-#            break
 
 def decode_type(name, type):
     global encoded_types
-    
+
     list_types = re.split('\d+', type)
     if list_types[0][0] in '{[(':
         proto = list_types[0]
     else:
         proto = encoded_types.get(list_types[0], 'unknown ' + list_types[0])
-        
+
     proto += ' ' + name + '('
     for t in list_types[3:]:
         if not t:
--- objc_helper.py
+++ objc_helper.py
@@ -8,21 +8,21 @@ var_re = re.compile(', \[SP,#0x.*\+(?P<varname>\w+)\]')
 def trace_param(ea, min_ea, op_type, op_val):
     '''
     trace_param: ea, min_ea, op_type, op_val
-    
+
     Taking ea as start, this function does basic backtrace of
     an operand (defined by op_type and op_val) until it finds
     a data reference which we consider the "source". It stops
     when ea < min_ea (usually the function start).
-    
+
     It does not support arithmetic or complex modifications of
     the source. This will be improved on future versions.
     '''
     global displ_re, msgsend, var_re
-    
+
     ea_call = ea
     while ea != idc.BADADDR and ea != min_ea:
         ea = idc.PrevHead(ea, min_ea)
-        
+
         if op_type == idaapi.o_reg and op_val == 0 and idaapi.is_call_insn(ea):
             # We have a BL/BLX that will modify the R0
             # we're tracking
@@ -62,7 +62,7 @@ def trace_param(ea, min_ea, op_type, op_val):
                     try:
                         var_name = var_re.search(idc.GetDisasm(ea)).group('varname')
                     except:
-                        print '%08x: Unable to recognize variable'
+                        print '%08x: Unable to recognize variable' % ea
                         return None
 
                     while ea != idc.BADADDR and ea > min_ea:
@@ -81,7 +81,7 @@ def trace_param(ea, min_ea, op_type, op_val):
                             op_val = int(displ_re.search(idc.GetDisasm(ea)).group('regnum'))
                         except:
                             print '%08x: Unable to recognize register' % ea
-                            return None    
+                            return None
             elif idc.GetOpType(ea, src_op) == idc.o_reg:
                 # Direct reg-reg assignment
                 op_val = idc.GetOperandValue(ea, src_op)
@@ -89,29 +89,29 @@ def trace_param(ea, min_ea, op_type, op_val):
                 # We don't track o_phrase or other complex source operands :(
                 return None
     return None
-        
-    
+
+
 
 def fix_callgraph(msgsend, segname, class_param, sel_param):
     '''
     fix_callgraph: msgsend, segname, class_param, sel_param
-    
+
     Given the msgsend flavour address as a parameter, looks
     for the parameters (class and selector, identified by
     class_param and sel_param) and creates a new segment where
     it places a set of dummy calls named as classname_methodname
     (we use method instead of selector most of the time).
     '''
-    
+
     t1 = time.time()
     if not msgsend:
         print 'ERROR: msgSend not found'
-        return 
+        return
 
     total = 0
     resolved = 0
     call_table = dict()
-    
+
     for xref in idautils.XrefsTo(msgsend, idaapi.XREF_ALL):
         total += 1
         ea_call = xref.frm
@@ -120,45 +120,45 @@ def fix_callgraph(msgsend, segname, class_param, sel_param):
             continue
         ea = ea_call
         method_name_ea = trace_param(ea, func_start, idc.o_reg, sel_param)
-        if method_name_ea:
+        if method_name_ea and idc.isASCII(idc.GetFlags(method_name_ea)):
             method_name = idc.GetString(method_name_ea, -1, idc.ASCSTR_C)
             if not method_name:
-                method_name = ''
+                method_name = '_unk_method'
         else:
-            method_name = ''
+            method_name = '_unk_method'
 
         class_name_ea = trace_param(ea, func_start, idc.o_reg, class_param)
         if class_name_ea:
             class_name = idc.Name(class_name_ea)
             if not class_name:
-                class_name = ''
+                class_name = '_unk_class'
         else:
-            class_name = ''
-            
-        if not method_name and not class_name:
+            class_name = '_unk_class'
+
+        if method_name == '_unk_method' and class_name == '_unk_class':
             continue
 
         # Using this name convention, if the class and method
         # are identified by IDA, the patched call will point to
         # the REAL call and not one of our dummy functions
-        # 
+        #
         class_name = class_name.replace('_OBJC_CLASS_$_', '')
         class_name = class_name.replace('_OBJC_METACLASS_$_', '')
         new_name = '_[' + class_name + '_' + method_name + ']'
-#        print '%08x: %s' % (ea_call, new_name)
+        print '%08x: %s' % (ea_call, new_name)
         call_table[ea_call] = new_name
         resolved += 1
-            
+
     print '\nFinal stats:\n\t%d total calls, %d resolved' % (total, resolved)
     print '\tAnalysis took %.2f seconds' % (time.time() - t1)
 
     if resolved == 0:
         print 'Nothing to patch.'
         return
-        
+
     print 'Adding new segment to store new nullsubs'
 
-    # segment size = opcode ret (4 bytes) * num_calls  
+    # segment size = opcode ret (4 bytes) * num_calls
     seg_size = resolved * 4
     seg_start = idc.MaxEA() + 4
     idaapi.add_segm(0, seg_start, seg_start + seg_size, segname, 'CODE')
@@ -183,7 +183,7 @@ def fix_callgraph(msgsend, segname, class_param, sel_param):
             if offset > 0 and offset & 0xFF800000:
                 print 'Offset too far for Thumb (%08x) Stopping [%08x]' % (offset, ea)
                 return
-    
+
             off1 = (offset & 0x7FF000) >> 12
             off2 = (offset & 0xFFF) / 2
             w1 = (0xF000 | off1)
@@ -196,23 +196,25 @@ def fix_callgraph(msgsend, segname, class_param, sel_param):
             dw = (0xFA000000 | (offset - 8 >> 2))
             if dw < 0:
                 dw = dw & 0xFAFFFFFF
-            idc.PatchDword(ea, dw)                
+            idc.PatchDword(ea, dw)
 
 
 def make_offsets(segname):
-    segea = idc.SegByName(segname)
+    segea = idc.SegByBase(idc.SegByName(segname))
     segend = idc.SegEnd(segea)
-    
+
     while segea < segend:
         idc.OpOffset(segea, 0)
         ptr = idc.Dword(segea)
         idc.OpOffset(ptr, 0)
         segea += 4
-            
+
 if __name__ == '__main__':
+    print 'Preparing class references segments'
     make_offsets('__objc_classrefs')
     make_offsets('__objc_superrefs')
     idaapi.analyze_area(idc.MinEA(), idc.MaxEA())
+    print 'Fixing callgraph'
     fix_callgraph(idc.LocByName('_objc_msgSend'), 'msgSend', 0, 1)
     fix_callgraph(idc.LocByName('_objc_msgSendSuper2'), 'msgSendSuper', 3, 1)
     idaapi.analyze_area(idc.MinEA(), idc.MaxEA())
